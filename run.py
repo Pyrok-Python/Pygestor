8fb1b48bfc20f8c1f2a37dc6651cde8115981918
Orlando J Barbon Santos <pyro9709@gmail.com>
2026-05-24 21:48:45 -0400

    Secure debug mode in production

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 from app import create_app
+import os
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Por defecto False en producción. En local será True si existe en .env
+    is_debug = os.environ.get('DEBUG', 'False').lower() == 'true'
+    app.run(debug=is_debug)
